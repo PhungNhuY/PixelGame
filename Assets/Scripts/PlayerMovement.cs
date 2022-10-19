7342ebc72f06eb1ae32eb5d0b49445d0c3dd8c5a
PhungNhuY <yphung282@gmail.com>
2022-10-19 10:54:27 +0700

    add horizontal movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("space")){
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 10, 0);
+        float dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump")){
+            rb.velocity = new Vector2(rb.velocity.x, 10);
         }
     }
 }
