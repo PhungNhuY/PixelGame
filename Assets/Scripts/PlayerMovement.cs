a334d7cd7ffbd3e771cc1eab56423cd6e0e7eef3
phuongtruong472001 <71766470+phuongtruong472001@users.noreply.github.com>
2022-10-19 10:13:51 +0700

    add player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("space")){
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 10, 0);
+        }
+    }
+}
