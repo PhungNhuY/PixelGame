f7f8edeb14f1bf72c0eab4217ee3a6178aa30715
PhungNhuY <yphung282@gmail.com>
2022-11-21 14:04:11 +0700

    add level 1

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -7,7 +7,6 @@ public class ItemCollector : MonoBehaviour
 {
     private int cherries = 0;
     [SerializeField] private Text point;
-
     [SerializeField] private AudioSource collectionSoundEffect;
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.gameObject.CompareTag("Cherry")){
